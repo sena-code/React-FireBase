ead7881de9399878725e88e1c93544527c3aaf10
sena-code <sena.carbone@gmail.com>
2020-12-10 23:18:42 -0300

    Feito

---
+++ src/utils/firebaseConfig.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyDtIpxDSpPkL3g7JI11UyxEoSziJrSN4U0",
+    authDomain: "nyous-a5960.firebaseapp.com",
+    projectId: "nyous-a5960",
+    storageBucket: "nyous-a5960.appspot.com",
+    messagingSenderId: "622545868274",
+    appId: "1:622545868274:web:5cf20d4ac24199da4e876b"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebaseConfig;
